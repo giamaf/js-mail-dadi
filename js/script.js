1bd7537bfcfefab428c63e7062b3bf43e053c993
giamaf <gianluca.maffucci@hotmail.com>
2023-12-21 15:48:27 +0100

    js_added player and cpu randomization

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log('JS OK')
 // ANALISI
 
 // 1 - RECUPERO GLI EVENTUALI ELEMENTI DAL DOM
-// 2 - AL CLICK SUL TASTO "RANDOM" GENERO DUE NUMERI CASUALI (PLAYER,PC) TRA 1 e 6.
+// 2 - AL CLICK SUL TASTO "RANDOM" GENERO DUE NUMERI CASUALI (PLAYER,PC) TRA 1 e 6
 // 3 - VALIDO L'OPERAZIONE PER STABILIRE UN VINCITORE
 // 4 - STAMPO IN PAGINA GLI ELEMENTI
 
@@ -21,3 +21,10 @@ const winnerName = document.getElementById('winner-name');
 const randomButton = document.getElementById('random-button');
 
 
+// 2 - AL CLICK SUL TASTO "RANDOM" GENERO DUE NUMERI CASUALI (PLAYER,PC) TRA 1 e 6
+
+randomButton.addEventListener('click', function () {
+    const randomPlayer = Math.floor(Math.random() * 6) + 1;
+    const randomCPU = Math.floor(Math.random() * 6) + 1;
+
+})
